aaf95fed571d3777a067791613b003284939a859
Jeremi Lorenti <120612878+JeremiLorenti@users.noreply.github.com>
2023-12-28 13:46:13 -0500

    Update settings window labels and placeholders

--- settings_window.py
+++ settings_window.py
@@ -22,25 +22,25 @@ class SettingsWindow:
         link_label.bind("<Button-1>", lambda e: webbrowser.open_new("https://platform.openai.com/api-keys"))
 
         # OpenAI API Settings
-        openai_api_label = ctk.CTkLabel(self.settings_root, text="OpenAI API Key:", font=("Lato", 10))
+        openai_api_label = ctk.CTkLabel(self.settings_root, text="Enter your OpenAI API key (required):", font=("Lato", 10))
         openai_api_label.pack(pady=(10, 0))
-        self.openai_api_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter your OpenAI API key", font=("Lato", 10))
+        self.openai_api_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter your OpenAI API key here. You can obtain it from your OpenAI account.", font=("Lato", 10))
         self.openai_api_entry.pack(pady=(0, 10))
 
         # WordPress Settings
-        wordpress_url_label = ctk.CTkLabel(self.settings_root, text="WordPress URL:", font=("Lato", 10))
+        wordpress_url_label = ctk.CTkLabel(self.settings_root, text="Enter your WordPress site URL (required):", font=("Lato", 10))
         wordpress_url_label.pack(pady=(10, 0))
-        self.wordpress_url_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter your WordPress URL", font=("Lato", 10))
+        self.wordpress_url_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter the URL of your WordPress site here.", font=("Lato", 10))
         self.wordpress_url_entry.pack(pady=(0, 10))
 
-        wordpress_username_label = ctk.CTkLabel(self.settings_root, text="WordPress Username:", font=("Lato", 10))
+        wordpress_username_label = ctk.CTkLabel(self.settings_root, text="Enter your WordPress username (required):", font=("Lato", 10))
         wordpress_username_label.pack(pady=(10, 0))
-        self.wordpress_username_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter your WordPress username", font=("Lato", 10))
+        self.wordpress_username_entry = ctk.CTkEntry(self.settings_root, width=300, placeholder_text="Enter your WordPress username here.", font=("Lato", 10))
         self.wordpress_username_entry.pack(pady=(0, 10))
 
-        wordpress_password_label = ctk.CTkLabel(self.settings_root, text="WordPress Password:", font=("Lato", 10))
+        wordpress_password_label = ctk.CTkLabel(self.settings_root, text="Enter your WordPress password (required):", font=("Lato", 10))
         wordpress_password_label.pack(pady=(10, 0))
-        self.wordpress_password_entry = ctk.CTkEntry(self.settings_root, width=300, show="*", placeholder_text="Enter your WordPress password", font=("Lato", 10))
+        self.wordpress_password_entry = ctk.CTkEntry(self.settings_root, width=300, show="*", placeholder_text="Enter your WordPress password here.", font=("Lato", 10))
         self.wordpress_password_entry.pack(pady=(0, 10))
 
         # Load existing settings and populate the fields
@@ -69,7 +69,7 @@ class SettingsWindow:
             # Show an error message using standard tkinter messagebox
             msgbox.showerror("Error", f"An error occurred while saving settings: {e}")
 
-    def center_window(self, width=500, height=400):  # Adjust the size as needed
+    def center_window(self, width=500, height=500):  # Adjust the size as needed
         # Get screen width and height
         screen_width = self.settings_root.winfo_screenwidth()
         screen_height = self.settings_root.winfo_screenheight()
